09e3f61cc2b4e300ce71c07646a680cdeef98fbe
ssz <ssz031117@163.com>
2025-05-17 19:38:05 +0800

    ci: 更新测试结果上传工具版本并刷新程序集信息版本号- 将 actions/upload-artifact工具从 v3 升级到 v4 - 更新 SSZ.Infrastructure 程序集的信息版本号

--- src/SSZ.Infrastructure/obj/Debug/net9.0/SSZ.Infrastructure.AssemblyInfo.cs
+++ src/SSZ.Infrastructure/obj/Debug/net9.0/SSZ.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SSZ.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5488dee0c0f5bd8ec308c40db2b90ea09529ccd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+271990d85c1785a25dd9fe8c8c5fede702d740d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SSZ.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SSZ.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
