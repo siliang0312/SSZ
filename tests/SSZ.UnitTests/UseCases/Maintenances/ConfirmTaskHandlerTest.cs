f768a62ebde01918fab43e7e07ff864aa1122af8
彭思亮 <2629145684@qq.com>
2025-05-26 10:19:34 +0800

    test:完善确认任务测试用例
    - 添加新的测试用例以验证任务通过和不通过的状态变化
    - 修复现有测试用例中的错误断言
    - 优化测试代码结构，提高可读性和可维护性

--- tests/SSZ.UnitTests/UseCases/Maintenances/ConfirmTaskHandlerTest.cs
+++ tests/SSZ.UnitTests/UseCases/Maintenances/ConfirmTaskHandlerTest.cs
@@ -15,23 +15,73 @@ public class ConfirmTaskHandlerTest
     ConfirmTaskCommand command,
     MaintenanceTask task)
   {
-    task.Id=command.Tasks.First().TaskId;
     MaintenanceTask[] maintenanceTasks = [task];
-    task.MarkSubmitted(Guid.NewGuid(),  1,"测试");
     taskRepository
       .ListAsync(Arg.Any<ISpecification<MaintenanceTask>>(), Arg.Any<CancellationToken>())
       .Returns(maintenanceTasks.ToList());
-    
-    
     var handler = new ConfirmTaskHandler(taskService,taskRepository);
-    
     
     await handler.Handle(command, CancellationToken.None);
     
     await taskRepository.Received()
       .ListAsync(Arg.Any<ISpecification<MaintenanceTask>>(), Arg.Any<CancellationToken>());
     await taskRepository.Received().SaveChangesAsync();
-    Assert.All(maintenanceTasks, a => Assert.NotEqual(a.State, TaskState.Submitted));
     await taskService.Received().ConfirmTask(Arg.Any<List<MaintenanceTask>>());
+  }
+  
+  [Theory, AutoNSubstituteData]
+  public async Task Handle_WhenPass_SateShouldConfirm(
+    [Frozen]IRepository<MaintenanceTask>  taskRepository,
+    [Frozen]IMaintenanceTaskService  taskService,
+    ConfirmTaskCommand command,
+    MaintenanceTask task)
+  {
+    task.Id=command.Tasks.First().TaskId;
+    for (int i = 0; i < command.Tasks.Count; i++)
+    {
+      var newTask = command.Tasks[i] with { IsPassed = true };
+      command.Tasks[i] = newTask;
+    }
+
+    MaintenanceTask[] maintenanceTasks = [task];
+    task.MarkSubmitted(Guid.NewGuid(),  1,"测试");
+    taskRepository
+      .ListAsync(Arg.Any<ISpecification<MaintenanceTask>>(), Arg.Any<CancellationToken>())
+      .Returns(maintenanceTasks.ToList());
+    
+    var handler = new ConfirmTaskHandler(taskService,taskRepository);
+    
+    await handler.Handle(command, CancellationToken.None);
+    
+   
+    Assert.All(maintenanceTasks, a => Assert.Equal(a.State, TaskState.Confirmed));
+  }
+  
+  [Theory, AutoNSubstituteData]
+  public async Task Handle_WhenNotPass_SateShouldSubmitted(
+    [Frozen]IRepository<MaintenanceTask>  taskRepository,
+    [Frozen]IMaintenanceTaskService  taskService,
+    ConfirmTaskCommand command,
+    MaintenanceTask task)
+  {
+    task.Id=command.Tasks.First().TaskId;
+    for (int i = 0; i < command.Tasks.Count; i++)
+    {
+      var newTask = command.Tasks[i] with { IsPassed = false };
+      command.Tasks[i] = newTask;
+    }
+
+    MaintenanceTask[] maintenanceTasks = [task];
+    task.MarkSubmitted(Guid.NewGuid(),  1,"测试");
+    taskRepository
+      .ListAsync(Arg.Any<ISpecification<MaintenanceTask>>(), Arg.Any<CancellationToken>())
+      .Returns(maintenanceTasks.ToList());
+    
+    var handler = new ConfirmTaskHandler(taskService,taskRepository);
+    
+    await handler.Handle(command, CancellationToken.None);
+    
+   
+    Assert.All(maintenanceTasks, a => Assert.Equal(a.State, TaskState.Waiting));
   }
 }
